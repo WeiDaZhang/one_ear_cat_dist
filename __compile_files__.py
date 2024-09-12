5378e4e9b10ded14e1f021339ce1bb0ffa63b6e0
WeiDaZhang <weida.zhang@outlook.com>
2024-09-12 23:19:44 +0100

    remove compile file list

--- __compile_files__.py
+++
@@ -1,9 +0,0 @@
-compile_files = [   'camera.py',
-                    'coordinate_calib.py',
-                    'coordinate_trans.py',
-                    'distance_from_focus.py',
-                    'image_process.py',
-                    'live_update_funcs.py',
-                    'probe_search.py',
-                    'pvcam.py',
-                    'threshold_edge_contour.py'  ]
